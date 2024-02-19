70d7f67eae1fda14e0c3a579aff827f69be88203
bugfish\bugfishtm <bugfish@bugfish.eu>
2024-02-19 03:45:47 +0100

    Initial

---
+++ README.md
@@ -0,0 +1,55 @@
+![Bugfish](https://img.shields.io/badge/Bugfish-FP2_Module-orange)
+![Status](https://img.shields.io/badge/Status-Finished-green)
+![License](https://img.shields.io/badge/License-GPLv3-black)
+![Version](https://img.shields.io/badge/Version-1.101-white)
+
+# Fast PHP Page Module
+## Documentation for AdminBSB integrated in FP2
+Template version Informations:
+[![npm version](https://img.shields.io/npm/v/adminbsb-materialdesign.svg)](https://www.npmjs.com/package/adminbsb-materialdesign)
+![Bower version](https://img.shields.io/bower/v/adminbsb-materialdesign.svg)
+
+**AdminBSB - Material Design is a fully responsive and free admin template.** It was developed with [Bootstrap 3.x Framework](http://getbootstrap.com) and [Google Material Design](https://material.google.com) of powers.
+
+It's really easy to customize and usage. You can use for from 320px to large resolution pixels of new generation screens and mobile devices. (**Fully Responsive Material Design**) 
+
+**This project is a module for the fast-php-page cms to always have the documentation for this template in hand! You can download this module out of your fast-php-page store if you are using the _administrator module!**
+
+## Installation
+To install this Fast PHP Page Module you have 2 options.
+
+### Manual Installation
+- Download this repository
+- Unpack the zip file
+- Put the folder in the zip file into your websites _site directory.
+- You can now execute the site with an admin module with the functionality to change to a website module or by using commands in cfg_ruleset.php
+
+### Download from Store
+- Login to fast PHP Page _administrator module
+- Go to the Store
+- Search for this module
+- Download and install the modul in the administrator interface
+
+## Repository Structure
+- `.github`: Contains files related to sponsorship information.
+- _releases: Releases of the software, which are installable modules for FP2 CMS.
+- _images: Images for the readme file
+
+## Reporting Issues
+If you encounter issues or have questions while using this software, please don't hesitate to contact us on our forum at www.bugfish.eu/forum.
+
+## Documentations
+Documentation Bugfish Framework:  
+https://www.bugfish-github.de/bugfish-framework  
+Documentation Fast-PHP-Page:  
+https://www.bugfish-github.de/fast-php-page    
+
+## 3rd Party Libraries
+In this project there has been used different 3rd party libraries (for example jQuery). All external Libraries are included in the websites /_vendor folder, as theire licenses. The project itself is made with the help of the "Bugfish Framework" and the "Fast PHP Page" Backend CMS.
+
+![Bugfish Framework](./_images/bugfish-framework-banner.jpg) 
+![Bugfish FP² Banner](./_images/bugfish-fp2-banner.jpg)
+
+## License
+
+For licensing information, please refer to the `license.md` file inside this repository's folder.
---
+++ _documentation-adminbsb/version.php
@@ -0,0 +1,49 @@
+<?php
+	/* 	__________ ____ ___  ___________________.___  _________ ___ ___  
+		\______   \    |   \/  _____/\_   _____/|   |/   _____//   |   \ 
+		 |    |  _/    |   /   \  ___ |    __)  |   |\_____  \/    ~    \
+		 |    |   \    |  /\    \_\  \|     \   |   |/        \    Y    /
+		 |______  /______/  \______  /\___  /   |___/_______  /\___|_  / 
+				\/                 \/     \/                \/       \/  	
+							www.bugfish.eu
+							
+	    Bugfish Fast PHP Page Framework
+		Copyright (C) 2024 Jan Maurice Dahlmanns [Bugfish]
+
+		This program is free software: you can redistribute it and/or modify
+		it under the terms of the GNU General Public License as published by
+		the Free Software Foundation, either version 3 of the License, or
+		(at your option) any later version.
+
+		This program is distributed in the hope that it will be useful,
+		but WITHOUT ANY WARRANTY; without even the implied warranty of
+		MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+		GNU General Public License for more details.
+
+		You should have received a copy of the GNU General Public License
+		along with this program.  If not, see <https://www.gnu.org/licenses/>.
+	*/	
+	$x = array();
+	$x["rname"] 		= "_documentation-adminbsb";
+	$x["build"] 		= "101";
+	$x["version"] 		= "1.".$x["build"];
+	$x["iframe"] 		= true;
+	$x["view"] 			= true;
+	$x["singleuse"] 	= false;
+	$x["cat"] 			= "docs";
+	$x["lang"] 			= array("en");
+	$x["license"] 		= "MIT";
+	$x["autor"] 		= "Jan-Maurice Dahlmanns";
+	$x["pseudo"] 		= "Bugfish";
+	$x["mail"] 			= "requests@bugfish.eu";
+	$x["website"] 		= "www.bugfish.eu";
+	$x["name"] 			= "AdminBSB Documentation";
+	$x["short"] 		= "Enhance your CMS experience with the AdminBSB Documentation, a comprehensive documentation companion. Seamlessly integrated into your admin interface, it provides instant access to detailed guides, tutorials, and reference materials. Streamline your workflow and empower users with essential knowledge for optimizing the potential of AdminBSB.";
+	$x["description"] 	= "Take your CMS journey to the next level with the AdminBSB Documentation, a meticulously crafted addition designed exclusively for the renowned AdminBSB admin template. Immerse yourself in an unparalleled documentation experience that seamlessly integrates into the AdminBSB interface, providing users with a comprehensive knowledge repository.
+	<br /><br />
+	Delve into the intricate details of AdminBSB's versatile components, responsive design, and Bootstrap foundation. Navigate through extensive guides that unravel the intricacies of customization, ensuring that every feature is wielded effectively. From quick tutorials for beginners to advanced tips for seasoned developers, the AdminBSB Documentation caters to all skill levels.
+	<br /><br />
+	Stay ahead of the curve with real-world examples, best practices, and troubleshooting guides, ensuring a smooth and productive development process. Whether you're crafting a sleek dashboard, implementing data tables, or refining the user experience, this extension equips you with the insights needed to master AdminBSB's capabilities.
+	<br /><br />
+	Empower your team with a shared knowledge base, fostering collaboration and accelerating project timelines. The AdminBSB Documentation isn't just a tool; it's a gateway to unlocking the full potential of AdminBSB, making your CMS experience more intuitive, efficient, and rewarding. Elevate your development journey today with the ultimate documentation companion.<br /><br />
+	<a href='https://github.com/gurayyarar/AdminBSBMaterialDesign' rel='noopener' target='_blank'>Visit the Projects Github Page!</a>";
